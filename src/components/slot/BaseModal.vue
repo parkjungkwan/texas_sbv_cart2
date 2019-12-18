323fa67c11346784ef2e20de260e1e542063cd75
parkjungkwan <pakjkwan@gmail.com>
2019-12-18 12:20:31 +0900

    12:20

---
+++ src/components/slot/BaseModal.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <header>
+    <slot name="header">
+      <strong>기본 타이틀</strong>
+      <button>기본 버튼</button>
+    </slot>
+  </header>
+  <div>
+    <slot name="content">
+      <p>기본 콘텐츠</p>
+    </slot>
+  </div>
+</div>
+</template>
+<script></script>
+<style scoped></style>
